e086fda59b3d60338d438171c9e9be4dcfc2b853
clinton1719 <clintoncleetus17@gmail.com>
2023-11-21 16:21:42 +0530

    Adding backend

---
+++ backend/src/test/java/com/b2b/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b2b.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
